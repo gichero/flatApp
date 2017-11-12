0febc92fb1445857b0e45f5e92fe712259c49952
Amos <amosgathungu@gmail.com>
2017-11-12 13:29:01 -0500

    first commit

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+
+    const config = {
+        apiKey: "AIzaSyD1igByDNK6foguQ4yZNsRA0xFhNfza0YY",
+        authDomain: "flata-a8fe4.firebaseapp.com",
+        databaseURL: "https://flata-a8fe4.firebaseio.com",
+        projectId: "flata-a8fe4",
+        storageBucket: "flata-a8fe4.appspot.com",
+        messagingSenderId: "467861912951"
+    };
+
+firebase.initializeApp(config);
+export default firebase;
