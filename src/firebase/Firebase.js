135aee6a87d41e426ea8f227ddf663c786afbbeb
Amos <amosgathungu@gmail.com>
2017-11-27 19:57:30 -0500

    not connected to backend

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
     const config = {
-        apiKey: "AIzaSyD1igByDNK6foguQ4yZNsRA0xFhNfza0YY",
-        authDomain: "flata-a8fe4.firebaseapp.com",
-        databaseURL: "https://flata-a8fe4.firebaseio.com",
-        projectId: "flata-a8fe4",
-        storageBucket: "flata-a8fe4.appspot.com",
-        messagingSenderId: "467861912951"
+        apiKey: 'AIzaSyD1igByDNK6foguQ4yZNsRA0xFhNfza0YY',
+        authDomain: 'flata-a8fe4.firebaseapp.com',
+        databaseURL: 'https://flata-a8fe4.firebaseio.com',
+        projectId: 'flata-a8fe4',
+        storageBucket: 'flata-a8fe4.appspot.com',
+        messagingSenderId: '467861912951'
     };
 
 firebase.initializeApp(config);
--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -16,8 +16,8 @@ import {
   Footer
 } from "native-base";
 import { Field, reduxForm } from "redux-form";
-//import firebase from "../../firebase/Firebase";
-import firebase from 'firebase';
+import firebase from "../../firebase/Firebase";
+//import firebase from 'firebase';
 
 import styles from "./styles";
 import commonColor from "../../theme/variables/commonColor";
@@ -49,17 +49,6 @@ class SignUpForm extends React.Component {
             password: '',
             error: ''
         };
-    }
-
-    componentWillMount(){
-        firebase.initializeApp({
-            apiKey: "AIzaSyD1igByDNK6foguQ4yZNsRA0xFhNfza0YY",
-            authDomain: "flata-a8fe4.firebaseapp.com",
-            databaseURL: "https://flata-a8fe4.firebaseio.com",
-            projectId: "flata-a8fe4",
-            storageBucket: "flata-a8fe4.appspot.com",
-            messagingSenderId: "467861912951"
-        });
     }
 
   textInput: any;
